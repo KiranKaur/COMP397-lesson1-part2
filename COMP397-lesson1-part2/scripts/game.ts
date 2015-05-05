324829eb57b3325e80587b67a1208076b6bc80b8
KiranKaur <k.angel117@yahoo.com>
2015-05-05 09:55:11 -0400

    https://github.com/KiranKaur/COMP397-lesson1-part2.git

---
+++ COMP397-lesson1-part2/scripts/game.ts
@@ -0,0 +1,18 @@
+﻿class Person {
+    // CONSTRUCTOR...
+    constructor() {
+
+    }
+    //PUBLIC Methods....
+    public sayHello() {
+        console.log("Hello world");
+    }
+} 
+
+function init() {
+
+    var myPerson = new Person();
+    myPerson.sayHello();
+
+}
+
